e4ae7a4b9d3e2ebeecf948c6a497193ef7e32d1e
Himanshu <kumarhimanshu2219@gmail.com>
2022-02-02 13:10:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import './App.css'
+import {Button} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+       <div className="App">
+           <h4>E Commerce Website</h4>
+           <button>Click here</button>
+           <Button>bootstrap btn</Button>
+       </div>
+    )
 }
 
 export default App;
